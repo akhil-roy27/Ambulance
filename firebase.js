1d0111666ac18f2a3054e60d75d17be05ad6cb5e
Goutham-Sreeram <goutham0705031@gmail.com>
2024-12-16 15:09:56 +0530

    firebase

--- firebase.js
+++ firebase.js
@@ -2,23 +2,16 @@ import { initializeApp } from 'firebase/app';
 import { GoogleAuthProvider, getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-
-  apiKey: "AIzaSyDqEZ8XQh8W1UNMxYmV8ckUfHYvJp2ge1k",
-
-  authDomain: "uber-clone-ddb16.firebaseapp.com",
-
-  projectId: "uber-clone-ddb16",
-
-  storageBucket: "uber-clone-ddb16.appspot.com",
-
-  messagingSenderId: "1062829683379",
-
-  appId: "1:1062829683379:web:3057427abacc8c0f9ab6c6"
-
+  apiKey: "AIzaSyCuToTr4R4tfOAY3Hswe_qN6I2RPq3YVc8",
+  authDomain: "uber-akhil.firebaseapp.com",
+  projectId: "uber-akhil",
+  storageBucket: "uber-akhil.firebasestorage.app",
+  messagingSenderId: "1001962582377",
+  appId: "1:1001962582377:web:23ff93ef33a6935c728df8",
+  measurementId: "G-Q7EJ1S267H"
 };
 
 // Initialize Firebase
-
 const app = initializeApp(firebaseConfig);
 const provider = new GoogleAuthProvider();
 const auth = getAuth();
