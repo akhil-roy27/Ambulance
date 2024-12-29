6fe839e5b5987d6b1681116e633d08d400583608
akhil-Roy27 <akhilroyp541@gmail.com>
2024-12-29 12:49:19 +0530

    changed firebase

--- firebase.js
+++ firebase.js
@@ -1,19 +1,21 @@
 import { initializeApp } from 'firebase/app';
 import { GoogleAuthProvider, getAuth } from 'firebase/auth';
+import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCuToTr4R4tfOAY3Hswe_qN6I2RPq3YVc8",
-  authDomain: "uber-akhil.firebaseapp.com",
-  projectId: "uber-akhil",
-  storageBucket: "uber-akhil.firebasestorage.app",
-  messagingSenderId: "1001962582377",
-  appId: "1:1001962582377:web:23ff93ef33a6935c728df8",
-  measurementId: "G-Q7EJ1S267H"
+  apiKey: "AIzaSyAsFErXQhdrNNNZvjxtlJRWuEfcaCukV8M",
+  authDomain: "ambulance-booking-app-a7235.firebaseapp.com",
+  projectId: "ambulance-booking-app-a7235",
+  storageBucket: "ambulance-booking-app-a7235.firebasestorage.app",
+  messagingSenderId: "986274133717",
+  appId: "1:986274133717:web:45d4316f68aec7e5ba545c",
+  measurementId: "G-0G0K8NVD0N"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const provider = new GoogleAuthProvider();
 const auth = getAuth();
+const analytics = getAnalytics(app);
 
-export { app, provider, auth };
+export { app, provider, auth, analytics };
